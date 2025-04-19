9efe4a878245556b9bb9d6b746bb0310a13f0ccf
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-04-20 00:31:42 +0100

    updt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function HomePage() {
-    const [fullScreenView, setFullScreenView] = useState();
-    const toggleFullScreen = () => {
-        setFullScreenView(!fullScreenView)
-    }
+    // const [fullScreenView, setFullScreenView] = useState();
+    // const toggleFullScreen = () => {
+    //     setFullScreenView(!fullScreenView)
+    // }
     return (
         <div className="home-page">
             <header className="home-hero">
@@ -36,17 +36,27 @@ export default function HomePage() {
                                 <div className="winter-blurry"></div>
                             </div>
                         </div>
-                        <div className="winter-bg" onClick={toggleFullScreen}></div>
+                        <div className="winter-bg"></div>
                     </div>
                     <div className="winter-potrait"></div>
-                    <div className={`test ${fullScreenView ? "show" : ""}`}>
-                        <div className="togle" onClick={toggleFullScreen}>
-                            <span className="first"></span>
-                            <span className="second"></span>
-                        </div>
-                        <div className="test-bg"></div>
-                    </div>
                 </section>
+                <div className="fall-collection">
+                    <div className="item item-1">
+                        <h1>fall collection</h1>
+                        <div>
+                            <p>
+                                Photos are metaphors, making the abstract concrete and mystifying the mundane. Be clear, be confident, and don’t overthink it. The beauty of your story is that it’s going to continue to evolve and your site can evolve with it. Your goal should be to make it feel right for right now. Later will take care of itself.
+                            </p>
+                            <p><a href="#">View full gallery</a></p>
+                        </div>
+                    </div>
+                    <div className="item item-2"></div>
+                    <div className="item item-3"></div>
+                    <div className="item item-4"></div>
+                    <div className="item item-5"></div>
+                    <div className="item item-6"></div>
+                    {/* <!-- <div class="item item-7">7</div> --> */}
+                </div>
             </main>
         </div>
     )
